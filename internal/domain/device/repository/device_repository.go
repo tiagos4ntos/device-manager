d351182b218af8dbc776165f8fc28f2ea69efe61
Tiago dos Santos <tiago482@gmail.com>
2025-08-28 22:19:35 -0300

    feat: device repository and unit tests implementation

--- internal/domain/device/repository/device_repository.go
+++ internal/domain/device/repository/device_repository.go
@@ -3,9 +3,8 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"errors"
-	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/tiagos4ntos/device-manager/internal/domain/device/entity"
 )
 
@@ -13,10 +12,11 @@ import (
 
 type DeviceRepository interface {
 	CreateDevice(ctx context.Context, device *entity.Device) error
-	GetDeviceByID(ctx context.Context, id string) (*entity.Device, error)
-	UpdateDevice(ctx context.Context, device *entity.Device) error
-	DeleteDevice(ctx context.Context, id string) error
-	ListDevices(ctx context.Context) ([]*entity.Device, error)
+	GetDeviceByID(ctx context.Context, id uuid.UUID) (entity.Device, error)
+	FullyUpdateDevice(ctx context.Context, device *entity.Device) error
+	UpdateDeviceState(ctx context.Context, deviceID uuid.UUID, newStatus entity.DeviceStatus) (entity.Device, error)
+	DeleteDevice(ctx context.Context, id uuid.UUID) error
+	ListDevices(ctx context.Context) ([]entity.Device, error)
 }
 
 type postegresDeviceRepository struct {
@@ -28,18 +28,14 @@ func NewDeviceRepository(db *sql.DB) *postegresDeviceRepository {
 }
 
 func (r *postegresDeviceRepository) CreateDevice(ctx context.Context, device *entity.Device) error {
-	if device == nil {
-		return errors.New("device is nil")
-	}
-
 	const query = `
-	INSERT INTO devices (id, name, brand, status)
+	INSERT INTO devices (id, name, brand, state)
 	VALUES ($1, $2, $3, $4)
-	RETURNING id, creation_time;`
+	RETURNING id, created_at, updated_at, deleted_at;`
 
 	statment, err := r.db.PrepareContext(ctx, query)
 	if err != nil {
-		return fmt.Errorf("prepare statement: %w", err)
+		return err
 	}
 	defer statment.Close()
 
@@ -48,33 +44,186 @@ func (r *postegresDeviceRepository) CreateDevice(ctx context.Context, device *en
 			device.ID,
 			device.Name,
 			device.Brand,
-			device.Status.String(),
+			device.State.String(),
 		).
-		Scan(&device.ID, &device.CreationTime)
+		Scan(&device.ID, &device.CreationTime, &device.UpdatedAt, &device.DeletedAt)
 
 	if err != nil {
-		return fmt.Errorf("scan row: %w", err)
+		return err
 	}
 
 	return nil
 }
 
-func (r *postegresDeviceRepository) GetDeviceByID(ctx context.Context, id string) (*entity.Device, error) {
-	//TODO: Implementation for retrieving a device by ID from Database
-	return nil, nil
+func (r *postegresDeviceRepository) GetDeviceByID(ctx context.Context, id uuid.UUID) (entity.Device, error) {
+	var device entity.Device
+
+	query := `
+	SELECT id, name, brand, state, created_at, updated_at, deleted_at
+	FROM devices
+	WHERE id = $1;`
+
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return device, err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRowContext(
+		ctx,
+		id.String(),
+	).Scan(
+		&device.ID,
+		&device.Name,
+		&device.Brand,
+		&device.State,
+		&device.CreationTime,
+		&device.UpdatedAt,
+		&device.DeletedAt)
+	if err != nil {
+		return device, err
+	}
+
+	return device, nil
 }
 
-func (r *postegresDeviceRepository) UpdateDevice(ctx context.Context, device *entity.Device) error {
-	//TODO: Implementation for updating a device in Database
+func (r *postegresDeviceRepository) FullyUpdateDevice(ctx context.Context, device *entity.Device) error {
+	query := `
+	UPDATE devices SET 
+		name = $2,
+		brand = $3,
+		state = $4,
+		updated_at = now()
+	WHERE id = $1 AND deleted_at IS NULL
+	RETURNING updated_at;`
+
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRowContext(
+		ctx,
+		device.ID,
+		device.Name,
+		device.Brand,
+		device.State.String(),
+	).Scan(
+		&device.UpdatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (r *postegresDeviceRepository) DeleteDevice(ctx context.Context, id string) error {
-	//TODO: Implementation for deleting a device in Database
+func (r *postegresDeviceRepository) UpdateDeviceState(ctx context.Context, deviceID uuid.UUID, newStatus entity.DeviceStatus) (entity.Device, error) {
+	var device entity.Device
+	query := `
+	UPDATE devices SET 
+		state = $2,
+		updated_at = now()
+	WHERE id = $1 AND deleted_at IS NULL
+	RETURNING id, name, brand, state, created_at, updated_at, deleted_at;`
+
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return device, err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRowContext(
+		ctx,
+		deviceID.String(),
+		newStatus.String(),
+	).Scan(
+		&device.ID,
+		&device.Name,
+		&device.Brand,
+		&device.State,
+		&device.CreationTime,
+		&device.UpdatedAt,
+		&device.DeletedAt,
+	)
+
+	if err != nil {
+		return device, err
+	}
+
+	return device, nil
+}
+
+func (r *postegresDeviceRepository) DeleteDevice(ctx context.Context, id uuid.UUID) error {
+	query := `UPDATE devices SET deleted_at = now() WHERE id = $1 AND deleted_at IS NULL AND state <> 'in-use';`
+
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(
+		ctx,
+		id)
+	if err != nil {
+		return err
+	}
+
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowCount <= 0 {
+		return sql.ErrNoRows
+	}
+
 	return nil
 }
 
-func (r *postegresDeviceRepository) ListDevices(ctx context.Context) ([]*entity.Device, error) {
-	//TODO: Implementation for listing all devices from Database
-	return nil, nil
+func (r *postegresDeviceRepository) ListDevices(ctx context.Context) ([]entity.Device, error) {
+	var devices []entity.Device
+
+	query := `SELECT id, name, brand, state, created_at, updated_at, deleted_at
+	FROM devices
+	ORDER BY name;`
+
+	stmt, err := r.db.PrepareContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.QueryContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var d entity.Device
+		err = rows.Scan(
+			&d.ID,
+			&d.Name,
+			&d.Brand,
+			&d.State,
+			&d.CreationTime,
+			&d.UpdatedAt,
+			&d.DeletedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		devices = append(devices, d)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return devices, nil
 }
